3909852f19741fc7581b7d630888ddf39c5452b4
Alexey Sokolov <alexey@ebi.ac.uk>
2020-12-13 12:33:32 +0000

    Adding postgress configs

--- proxy/app/main.py
+++ proxy/app/main.py
@@ -7,21 +7,32 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 es = Elasticsearch(['elasticsearch-master-headless:9200'])
 
 
-@app.route("/organism/<mode>")
+# @app.route("/organism/<mode>")
+# @cross_origin()
+# def bovreg_organisms(mode):
+#     if mode == 'private':
+#         data = es.search(index='organism', size=10000)
+#     else:
+#         data = es.search(index='organism', q="private:false", size=10000)
+#     return data
+#
+#
+# @app.route("/specimen")
+# @cross_origin()
+# def bovreg_specimens():
+#     data = es.search(index='specimen', size=10000)
+#     return data
+
+@app.route("/protocols_samples")
 @cross_origin()
-def bovreg_organisms(mode):
-    if mode == 'private':
-        data = es.search(index='organism', size=10000)
-    else:
-        data = es.search(index='organism', q="private:false", size=10000)
-    return data
+def all_records():
+    return es.search(index="protocols_samples", size=10000)
 
 
-@app.route("/specimen")
+@app.route("/protocols_samples/<record_id>")
 @cross_origin()
-def bovreg_specimens():
-    data = es.search(index='specimen', size=10000)
-    return data
+def details(record_id):
+    return es.search(index="protocols_samples", q=f"_id:{record_id}")
 
 
 if __name__ == "__main__":
